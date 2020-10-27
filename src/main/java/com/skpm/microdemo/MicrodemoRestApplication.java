4fd013275603b2fa59f1cd2732a870965d34b60c
Suresh Kumar Pun Magar <yayayokoho3@gmail.com>
2020-10-27 09:08:54 +0545

    First commit

---
+++ src/main/java/com/skpm/microdemo/MicrodemoRestApplication.java
@@ -0,0 +1,11 @@
+package com.skpm.microdemo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class MicrodemoRestApplication extends Application {
+}
