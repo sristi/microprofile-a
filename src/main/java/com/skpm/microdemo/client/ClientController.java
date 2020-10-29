c0a423a3e6c6b1fc4e076eb4ef42ccb3c5a56748
Suresh Kumar Pun Magar <yayayokoho3@gmail.com>
2020-10-29 21:23:33 +0545

    User Service added and made workable, difference is for get/Id it does as swagger type of working

--- src/main/java/com/skpm/microdemo/client/ClientController.java
+++ src/main/java/com/skpm/microdemo/client/ClientController.java
@@ -4,9 +4,11 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Path("/client")
 @ApplicationScoped
@@ -16,9 +18,60 @@ public class ClientController {
     @RestClient
     private Service service;
 
+    List<User> users = new ArrayList<User>();
     @GET
     @Path("/test/{parameter}")
     public String onClientSide(@PathParam("parameter") String parameter) {
         return service.doSomething(parameter);
+    }
+
+    @GET
+    @Path("/users")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<User> allUsers() {
+        return users;
+    }
+
+    @POST
+    @Path("/users")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<User> add(User user){
+        Long id = 0L;
+        if(users.size()>0) {
+            User userOfMaxId = users.stream().reduce((a, b) -> a.getId().compareTo(b.getId()) > 0 ? a : b).get();
+            //Long id = Long.valueOf(users.size()+1);
+            id = Long.valueOf(userOfMaxId == null ? 1 : userOfMaxId.getId() + 1);
+        }else
+            id = 1L;
+        user.setId(id);
+        /*user.setName("user"+id);
+        user.setPassword("pwd"+id);
+        user.setUserName("u"+id);*/
+        users.add(user);
+        return users;
+    }
+    @GET
+    @Path("/users/{uid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Optional<User> findById(@PathParam("uid") long uid){
+        return users.stream()
+                .filter(item->item.getId().equals(uid))
+                .findFirst();
+    }
+    @DELETE @Path("/users/{userId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<User> remove (@PathParam("userId") long id){
+        users.removeIf(usr->usr.getId().equals(id));
+        return users;
+    }
+
+    @GET
+    @Path("/usersById/{uid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User findUserById(@PathParam("uid") long uid){
+        return users.stream()
+                .filter(item->item.getId().equals(uid))
+                .findFirst().get();
     }
 }
---
+++ src/main/java/com/skpm/microdemo/client/User.java
@@ -0,0 +1,54 @@
+package com.skpm.microdemo.client;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.json.bind.annotation.JsonbProperty;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+
+public class User {
+    @Max(100000)
+    private Long id;
+    @NotBlank(message = "name can should not be blank")
+    private String name;
+    @NotBlank
+    @JsonProperty("user_name")
+    private String userName;
+    @NotBlank
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User() {}//added to fix Json Deserialization error No default constructor found
+}
+
